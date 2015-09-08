27b4a6fb481e7a9ab68b14ecf7e1a953bc78edac
Philip Williams <philipc.williams@icloud.com>
2015-09-08 14:22:20 -0500

    Create call model

---
+++ app/Call.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Call extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'cdr';
+}

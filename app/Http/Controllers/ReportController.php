e84a269899440c5566946e6ad910cab5b8581d04
Philip Williams <philipc.williams@icloud.com>
2015-09-14 11:01:55 -0500

    Add scratchpad and modified report controller and routes

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -3,11 +3,61 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+use DB;
+use App\Call;
+
 class ReportController extends Controller
 {
-    //
+
+	/**
+	 * [index description]
+	 * @return [type] [description]
+	 */
+	public function index()
+	{
+		return 'date picker';
+	}
+
+	/**
+	 * [index description]
+	 * @return [type] [description]
+	 */
+    public function report()
+    {
+    	dd(
+    		Call::select(DB::raw('account_codes.name, src, cdr.accountcode, count(cdr.accountcode), sum(billsec)'))
+		   ->join('account_codes', 'cdr.accountcode', '=', 'account_codes.accountcode')
+		   ->where('calldate', '>=', '2015-08-01 00:00:00')
+		   ->where('calldate', '<=', '2015-08-30 23:59:00')
+		   ->where('cdr.accountcode', '!=', '')
+		   ->where('outbound_cnum', '!=', '')
+		   ->where('disposition', '=', 'ANSWERED')
+		   ->groupBy('cdr.accountcode')
+		   ->orderByRaw('sum(billsec) desc')
+		   ->take(10)
+		   ->get()
+    	);
+    }
+
+    
+    /**
+     * [report description]
+     * @return [type] [description]
+     */
+    public function details()
+    {
+    	var_dump (
+    		Call::select('calldate', 'dst', 'billsec')
+			   ->where('calldate', '>=', '2015-08-01 00:00:00')
+			   ->where('calldate', '<=', '2015-08-30 23:59:00')
+			   ->where('accountcode', '=', '1866')
+			   ->where('outbound_cnum', '!=', '')
+			   ->where('disposition', '=', 'ANSWERED')
+			   ->orderBy('billsec', 'desc')
+			   ->get()
+    	);
+    }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,9 +17,8 @@
  *  / - show date picker.
  *  report - list top ten account codes with the most calls for the selected date range.
  *  report/{account-code} - list all calls for the selected account code within the specifed date range.
- *
  */
 
 Route::get('/', 'ReportController@index');
-Route::post('report', 'ReportController@report');
-Route::get('report/{account-code}', 'ReportController@details');
+Route::get('report', 'ReportController@report');
+Route::get('report/{accountcode}', 'ReportController@details');
